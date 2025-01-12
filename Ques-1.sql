79d087978eea5626003515c0cf1acacc788bd639
Tamizharasan <stamizh.code@gmail.com>
2025-01-12 19:14:34 +0530

    Add files via upload

--- Ques-1.sql
+++ Ques-1.sql
@@ -3,5 +3,5 @@ use joinx;
 create table patient(fname varchar(10),lname varchar(10),allergies varchar(10),gender char(1));
 insert into patient values('rajan','das','null','m'),('roky','bai', 'null','m'),
 ('maha','sri','yes','f'),('vishu','kutti','yes','f');
-select fname,lname from patient where allergies <> 'null' or 'no';
+select fname,lname from patient where gender='m';
 
